d579f6c3c4ee2e2bd6cff0f22301e79e21c00051
dory-finn <dorystudio.lab@gmail.com>
2026-04-03 01:54:42 +0800

    fix: fix the migration error

--- apps/web/lib/database/pglite/migrations/0005_wet_jack_power.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "connections" ALTER COLUMN "host" DROP NOT NULL;--> statement-breakpoint
-ALTER TABLE "connections" ALTER COLUMN "port" DROP NOT NULL;--> statement-breakpoint
-ALTER TABLE "connections" ADD COLUMN "path" text;
--- apps/web/lib/database/postgres/migrations/0005_free_sqlite_path.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "connections" ADD COLUMN "path" text;
-ALTER TABLE "connections" ALTER COLUMN "host" DROP NOT NULL;
-ALTER TABLE "connections" ALTER COLUMN "port" DROP NOT NULL;
